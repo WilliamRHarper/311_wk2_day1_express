7442d9ff18c7abacb3590377e2b0de1bd278ae48
WilliamRHarper <Wrharpe1230@gmail.com>
2020-07-16 22:41:28 -0500

    changes in logic

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ let counter = users.length;
 app.use(bodyParser());
 
 /* BEGIN - create routes here */
-app.get("/users", (req, res) => {
-  return res.json(users);
-});
-app.get("/users/1", (req, res) => {
-  return res.json(users[0]);
-});
+// app.get("/users", (req, res) => {
+//   return res.json(users);
+// });
+// app.get("/users/1", (req, res) => {
+//   return res.json(users[0]);
+// });
 app.post("/users", (req, res) => {
   users.push(newUser);
   return res.json(users[users.length -1]);
@@ -31,12 +31,38 @@ app.put("/users/1", (req, res) => {
   return res.json(users[0]);
 });
 app.delete("/users/1", (req, res) => {
-  users.shift(users[0]);
+  return res.json(users.slice(1));
+  // return res.json(newUsers);
+});
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  users.push({
+      _id: (counter +=1),
+      ...req.body
+    });
   return res.json(users);
 });
-app.post("/users", (req, res => {
-  console.log(req.body);
-}));
+app.get("/users/:userId", (req, res) => {
+  const id = req.params.userId;
+  const user = users.filter(u => u._id === Number(id));
+  return res.json(user);
+});
+app.put("/users/:userId", (req, res) => {
+  const id = req.params.userId;
+  const user = users.find(u => u._id === Number(id));
+  user.occupation = "slacker";
+  return res.json(user);
+});
+app.delete("/users/:userId", (req, res) => {
+  const id = req.params.userId;
+  for (let i = 0; i < users.length; i++){
+    if (users[i]._id === Number(id)){
+      let spliced = users.splice(i, 1);
+      spliced[0].isActive = false;
+    }
+  }
+  res.send("deleted");
+});
 
 
 
